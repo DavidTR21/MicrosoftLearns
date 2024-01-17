0a139bb8745df8b68efd9f55ebb58fb7feb5a477
David T. <davidtr.2105@gmail.com>
2024-01-17 12:41:11 +0100

    modified:   .NET/Concats/obj/Debug/net7.0/Concats.AssemblyInfo.cs 	modified:   .NET/Concats/obj/Debug/net7.0/Concats.AssemblyInfoInputs.cache 	modified:   .NET/Conditionals/obj/Conditionals.csproj.nuget.dgspec.json 	modified:   .NET/Conditionals/obj/Debug/net8.0/Conditionals.AssemblyInfo.cs 	modified:   .NET/Conditionals/obj/Debug/net8.0/Conditionals.AssemblyInfoInputs.cache 	modified:   .NET/Conditionals/obj/Debug/net8.0/Conditionals.GeneratedMSBuildEditorConfig.editorconfig 	modified:   .NET/Conditionals/obj/Debug/net8.0/Conditionals.assets.cache 	modified:   .NET/Conditionals/obj/project.nuget.cache 	modified:   .NET/ConditionalsChallenge/obj/ConditionalsChallenge.csproj.nuget.dgspec.json 	modified:   .NET/ConditionalsChallenge/obj/Debug/net8.0/ConditionalsChallenge.AssemblyInfo.cs 	modified:   .NET/ConditionalsChallenge/obj/Debug/net8.0/ConditionalsChallenge.AssemblyInfoInputs.cache 	modified:   .NET/ConditionalsChallenge/obj/Debug/net8.0/ConditionalsChallenge.GeneratedMSBuildEditorConfig.editorconfig 	modified:   .NET/ConditionalsChallenge/obj/Debug/net8.0/ConditionalsChallenge.assets.cache 	modified:   .NET/ConditionalsChallenge/obj/project.nuget.cache 	modified:   .NET/ConsoleApps/obj/Debug/net7.0/ConsoleApps.AssemblyInfo.cs 	modified:   .NET/ConsoleApps/obj/Debug/net7.0/ConsoleApps.AssemblyInfoInputs.cache 	modified:   .NET/ContactsChallenge/obj/Debug/net7.0/ContactsChallenge.AssemblyInfo.cs 	modified:   .NET/ContactsChallenge/obj/Debug/net7.0/ContactsChallenge.AssemblyInfoInputs.cache 	modified:   .NET/MethodCalls/obj/Debug/net8.0/MethodCalls.AssemblyInfo.cs 	modified:   .NET/MethodCalls/obj/Debug/net8.0/MethodCalls.AssemblyInfoInputs.cache 	modified:   .NET/MethodCalls/obj/Debug/net8.0/MethodCalls.GeneratedMSBuildEditorConfig.editorconfig 	modified:   .NET/MethodCalls/obj/Debug/net8.0/MethodCalls.assets.cache 	modified:   .NET/Operations/obj/Debug/net7.0/Operations.AssemblyInfo.cs 	modified:   .NET/Operations/obj/Debug/net7.0/Operations.AssemblyInfoInputs.cache 	modified:   .NET/OperationsChallenge/obj/Debug/net7.0/OperationsChallenge.AssemblyInfo.cs 	modified:   .NET/OperationsChallenge/obj/Debug/net7.0/OperationsChallenge.AssemblyInfoInputs.cache 	modified:   .NET/Projects/StudentGrading/obj/Debug/net7.0/StudentGrading.AssemblyInfo.cs 	modified:   .NET/Projects/StudentGrading/obj/Debug/net7.0/StudentGrading.AssemblyInfoInputs.cache 	modified:   .NET/Projects/StudentGrading2/obj/Debug/net7.0/StudentGrading2.AssemblyInfo.cs 	modified:   .NET/Projects/StudentGrading2/obj/Debug/net7.0/StudentGrading2.AssemblyInfoInputs.cache 	modified:   .NET/variables/obj/Debug/net7.0/variables.AssemblyInfo.cs 	modified:   .NET/variables/obj/Debug/net7.0/variables.AssemblyInfoInputs.cache

--- .NET/Operations/obj/Debug/net7.0/Operations.AssemblyInfo.cs
+++ .NET/Operations/obj/Debug/net7.0/Operations.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Operations")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c731248bb6e044845ca330f4671aa73a39136b50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b40ec2b210a7dde3b5f92e4c32a8341fe3ab4af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Operations")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Operations")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
